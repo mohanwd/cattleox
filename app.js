17debab6b40bc9a5560bb65f32fa1191d1ab3adf
mohanwd <mohan.k.wiredelta@gmail.com>
2017-05-03 14:30:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(window).bind('mousewheel', function(event) {
+/*$(window).bind('mousewheel', function(event) {
 if (event.originalEvent.wheelDelta >= 0) {
     $( "#bottom" ).slideDown( "slow", function() {
     // Animation complete.
@@ -15,4 +15,34 @@ else {
     // Animation complete.
   });
 }
-});
+});*/
+
+
+$(function(){
+      //Keep track of last scroll
+      var lastScroll = 0;
+      $(window).scroll(function(event){
+          //Sets the current scroll position
+          var st = $(this).scrollTop();
+          //Determines up-or-down scrolling
+          if (st > lastScroll){
+             //Replace this with your function call for downward-scrolling
+             $( "#bottom" ).slideUp( "slow", function() {
+				// Animation complete.
+			  });
+				$( "#top" ).slideUp( "slow", function() {
+				// Animation complete.
+			  });
+          }
+          else {
+             $( "#bottom" ).slideDown( "slow", function() {
+				// Animation complete.
+			  });
+				$( "#top" ).slideDown( "slow", function() {
+				// Animation complete.
+			  });
+          }
+          //Updates scroll position
+          lastScroll = st;
+      });
+    });
